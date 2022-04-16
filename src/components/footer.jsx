e97055b1a3477d072ccd833018625fcaa5537ae7
techmaxus <lakshay.mc19@nsut.ac.in>
2022-04-16 22:22:22 +0530

    fully functional

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './footer.css';
+const Footer = () => (
+  <footer className="footer">
+    <p>Developed by <a href="https://github.com/techmaxus" target="_blank">Lakshay Kapoor</a></p>
+  
+  </footer>
+);
+  
+export default Footer;
